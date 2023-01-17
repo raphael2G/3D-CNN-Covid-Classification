f331631047afe263edb9bfdc308825eba32e25f7
Derek <darmfield2023@gmail.com>
2023-01-16 23:31:03 -0500

    updated model creation + fitting

--- 2d-model-training.py
+++ 2d-model-training.py
@@ -4,21 +4,36 @@ import numpy as np
 from tensorflow.keras import layers
 
 
+# create the data
 (train_dataset, test_dataset), info = tfds.load('mnist', split=['train', 'test'], with_info=True, as_supervised=True, shuffle_files=True)
 tfds.as_dataframe(train_dataset.take(5), info)
 
-model = tf.Sequential([
+
+# create the model
+model = tf.keras.Sequential([
     tf.keras.layers.Input([28, 28, 3]), 
-    tf.keras.layers.Conv2D(3, 3, (3,3)), 
+    tf.keras.layers.Rescaling(scale=1./255),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)), 
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Conv2D(64,(3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'), 
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-model = tf.keras.Functional(
-    [
-        keras.Input(shape=input_shape),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.GlobalAveragePositioning()
-        layers.Dense(num_classes, activation="softmax"),
-    ]
-)
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+              metrics=tf.keras.metrics.SparseCategoricalAccuracy()
+             )
+model.summary()
+
+model.fit(train_dataset.batch(16), 1)
+
+
+
+
+
+
 
